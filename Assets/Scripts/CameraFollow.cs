180efc6c0003f4f3f294ac814235ecd05430cf21
Aditya Thoutam <adityathoutam@outlook.com>
2019-03-02 10:23:11 +0000

    No Comments

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Vector3 offset;
+    public GameObject Player;
+    
+    void Start()
+    {
+        offset = this.transform.position-Player.transform.position;       
+         
+    }
+
+    
+    void FixedUpdate()
+    {
+        this.transform.position = Player.transform.position+offset;
+        
+    }
+}
