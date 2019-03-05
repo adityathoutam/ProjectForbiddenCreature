ac97940903bfb608de60d7379e666a3e8bcdeee8
Aditya Thoutam <adityathoutam@outlook.com>
2019-03-05 11:07:06 +0000

    No Comments

---
+++ Assets/Scripts/BranchCollision.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BranchCollision : MonoBehaviour
+{
+   
+   public GameObject branchParent;
+
+    public Vector3 start;
+    public Vector3 end;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Weight")
+        {
+            StartCoroutine(Bridge(branchParent,start,end,2));
+        }
+    }
+
+    IEnumerator Bridge(GameObject obj,Vector3 start, Vector3 end, float time)
+    {
+        var i = 0.0f;
+        var rate = 1.0f/time;
+        while(i<1)
+        {
+            i += Time.deltaTime*rate;
+            if(obj!=null)
+            {
+                obj.transform.position = Vector2.Lerp(start,end,i);
+                Debug.Log("Afsa");
+            }
+        }
+
+        yield return null;
+    }
+}
