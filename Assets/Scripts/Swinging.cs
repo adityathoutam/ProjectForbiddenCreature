031110f8437dccc566556137b019c1f9d70e73b7
Sai Teja <saitejavenneti@gmail.com>
2019-03-10 10:38:57 +0000

    No Comments

---
+++ Assets/Scripts/Swinging.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swinging : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject player;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = player.GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(Input.GetKey(KeyCode.LeftControl))
+            {
+                anim.SetBool("Swing", true);
+                player.GetComponent<Rigidbody2D>().simulated = false;
+            }
+            else
+            {
+                anim.SetBool("Swing", false);
+                player.GetComponent<Rigidbody2D>().simulated = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
