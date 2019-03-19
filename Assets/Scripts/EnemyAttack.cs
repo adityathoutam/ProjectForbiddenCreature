f087792982691e4d67f7dd78369ba3b3eb30bfac
Sai Teja <saitejavenneti@gmail.com>
2019-03-19 05:36:14 +0000

    No Comments

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject player;
+
+    public GameObject enemy;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Vector2.Distance(enemy.transform.position,player.transform.position)<=2)
+        {
+            cam.GetComponent<CameraFollow>().enabled = false;
+        }
+    }
+}
